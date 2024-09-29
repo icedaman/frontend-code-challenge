3d63172fcbd31d96e999431df2d723e836f441e9
icedaman <36274654+icedaman@users.noreply.github.com>
2024-09-29 23:06:06 +0100

    Sort User by column name created

--- hansecom-challenge/src/components/UsersTable.vue
+++ hansecom-challenge/src/components/UsersTable.vue
@@ -12,19 +12,43 @@ const props = defineProps({
 });
 
 const searchFilter = ref('');
+const orderDescending = ref(true);
+const colToOrder = ref('');
 
 const handleSearch = (searchInput) => {
   searchFilter.value = searchInput;
 }
 
+const sort = (colName) => {
+  orderDescending.value = !orderDescending.value;
+  colToOrder.value = colName;
+}
+
+const sortByCol = (columnName) => {
+  let users = props.users;
+  
+  if(orderDescending.value){
+    users = users.sort( (a, b) => a[columnName] > b[columnName] ? -1 : b[columnName] > a[columnName] ? 1 : 0);
+  } else{
+    users = users.sort( (a, b) => a[columnName] > b[columnName] ? 1 : b[columnName] > a[columnName] ? -1 : 0);
+  }
+  
+  return users;
+}
+
 const filteredUsers = computed( () => {
+  let users = props.users;
+  
+  users = sortByCol(colToOrder.value);
+
   if(searchFilter.value !== ''){
-    return props.users.filter(user => (
+    users = props.users.filter( user => (
       user.fullName.toLowerCase().includes(searchFilter.value.toLowerCase()) ||
       user.email.toLowerCase().includes(searchFilter.value.toLowerCase())
     ));
   }
-  return props.users;
+
+  return users;
 })
 
 const deleteUser = (id) => {
@@ -40,12 +64,12 @@ const deleteUser = (id) => {
     <table class="w-full text-sm text-left text-gray-500">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50">
         <tr>
-          <th class="px-4 py-3">ID</th>
-          <th class="px-4 py-3">Full Name</th>
-          <th class="px-4 py-3">Email</th>
-          <th class="px-4 py-3 text-center">Update</th>
-          <th class="px-4 py-3 text-center">Delete</th>
-          <th class="px-4 py-3 text-center">Orders</th>
+          <th class="px-4 py-3 cursor-pointer hover:bg-slate-300" @click="sort('id')">ID {{ orderDescending ? '&darr;' : '&uarr;' }}</th>
+          <th class="px-4 py-3 cursor-pointer hover:bg-slate-300" @click="sort('fullName')">Full Name {{ orderDescending ? '&darr;' : '&uarr;' }}</th>
+          <th class="px-4 py-3 cursor-pointer hover:bg-slate-300" @click="sort('email')">Email {{ orderDescending ? '&darr;' : '&uarr;' }}</th>
+          <th class="px-4 py-3 text-center">Update User</th>
+          <th class="px-4 py-3 text-center">Delete User</th>
+          <th class="px-4 py-3 text-center">User Orders</th>
         </tr>
       </thead>
       <tbody>
@@ -54,10 +78,10 @@ const deleteUser = (id) => {
           <td class="px-4 py-3 font-medium text-gray-900">{{ user.fullName }}</td>
           <td class="px-4 py-3">{{ user.email }}</td>
           <td class="text-center text-white">
-            <RouterLink :to='"/user/" + user.id + "/edit"' class="px-4 py-2 bg-yellow-600 rounded-lg">Update User</RouterLink>
+            <RouterLink :to='"/user/" + user.id + "/edit"' class="px-4 py-2 bg-yellow-600 rounded-lg">Edit</RouterLink>
           </td>
           <td class="text-center text-white">
-            <button @click="deleteUser(user.id)" class="px-4 py-2 bg-red-700 rounded-lg">Delete User</button>
+            <button @click="deleteUser(user.id)" class="px-4 py-2 bg-red-700 rounded-lg">X</button>
           </td>
           <td class="text-center text-white">
             <RouterLink :to='"/orders/" + user.id' class="px-4 py-2 bg-blue-700 rounded-lg">Orders</RouterLink>
--- hansecom-challenge/src/views/EditUserView.vue
+++ hansecom-challenge/src/views/EditUserView.vue
@@ -58,7 +58,7 @@ onMounted( async ()=> {
     <div class="m-auto max-w-2xl py-24">
       <div class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
         <form @submit.prevent="handleSubmit">
-          <h2 class="text-3xl text-center font-semibold mb-6">Update User</h2>
+          <h2 class="text-3xl text-center font-semibold mb-6">Edit User</h2>
           <div class="mb-4">
             <label class="block text-gray-700 font-bold mb-2">Full Name</label>
             <input
